e7a0b750814d533f0f40636a49a32d1d9fefa2b6
Charles Marcilly <charles.peixoto@outlook.fr>
2023-07-03 16:29:05 +0200

    delete, update et liste de reservation

--- hotel/index.php
+++ hotel/index.php
@@ -1,22 +1,52 @@
-<?php include 'component/header.php'; ?> 
+<?php 
+include "inc.php";
+include 'component/header.php'; 
+
+//Récupération des chambres au niveau BD
+$stmt = executerRequete("SELECT * FROM chambre");
+
+//tableau pour stocker toutes les instances de chambre.
+$tabChambres = [];
+
+//on récupère un array sur chaque tour de boucle
+while( $res = $stmt->fetch() ){
+     extract($res);
+     $c = new Chambre($numChambre, $prix, $nbLits, $nbPers, $image, $description);
+
+     $tabChambres[] = $c;
+}
+
+// Update et Delete
+if(isset ($_GET['action'])){
+    $action = $_GET["action"];
+
+    switch($action){
+        case'delete' : 
+            echo "Cas delete";
+    }
+}
+
+?> 
 
 
 <div class="row">
 
 <?php
-for ($i=0; $i < 2 ; $i++): ?> 
+foreach ($tabChambres as $chambre): ?> 
      <div class="card mx-1" style="width: 15rem;">
-          <img class="card-img-top" src="img/c1.jpg" alt="Card image cap">
+          <img class="card-img-top" src="img/<?= $chambre->getImage(); ?>" alt="Card image cap">
           <div class="card-body">
-               <a href="chambre.php?idChambre=1" class="btn btn-outline-success my-1 w-100">Détail</a>
+               <a href="chambre.php?idChambre=<?= $chambre->getNumChambre(); ?>" class="btn btn-outline-success my-1 w-100">Détail</a>
 
-               <a href="chambre.php?idChambre=1" class="btn btn-outline-success my-1 w-100">Update</a>
-               <a href="chambre.php?idChambre=1" class="btn btn-outline-danger my-1 w-100">Delete</a>
-
+               <!-- access admin -->
+               <?php if( isset($_SESSION['user']) && unserialize($_SESSION['user'])->getRole() == "admin" ): ?>
+                    <a href="chambre.php?action=update&idChambre=<?= $chambre->getNumChambre(); ?>" class="btn btn-outline-success my-1 w-100">Update</a>
+                    <a href="chambre.php?action=delete&idChambre=<?= $chambre->getNumChambre(); ?>" class="btn btn-outline-danger my-1 w-100">Delete</a>
+               <?php endif; ?>
           </div>
      </div>
-<?php endfor; ?> 
+<?php endforeach; ?> 
 
 </div>
 
-<?php include 'component/footer.html.php';
+<?php include 'component/footer.php';
